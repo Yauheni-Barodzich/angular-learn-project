d180059ec91c3e6de4f9ead19e189408ed4aeeb7
YaheniBarodzich <yauheni_barodzich@epam.com>
2014-10-05 17:56:03 +0300

    fix jshint

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,27 +2,27 @@
     'use strict';
 
     angular.module('newsFrontendApp', [
-    'trainingnews'
-    ])
-	.config(['$routeProvider',
-		function($routeProvider) {
-			$routeProvider.
-			when('/content', {
-				templateUrl: 'views/components/news-item.html'
-			}).
-			when('/content/:newsId', {
-				templateUrl: 'views/components/news-detail.html'
-			}).
-            when('/posts', {
-                templateUrl: 'views/components/post-list.html',
-                controller: 'PostsListCtrl'
-            })
-            .when('/posts/:_id', {
-                templateUrl: 'views/components/post-detail.html',
-                controller: 'PostPageCtrl'
-            })
-            .otherwise({
-				redirectTo: '/content'
-			});
-	    }]);
-})();
+        'trainingnews'
+      ])
+    .config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+        when('/content', {
+            templateUrl: 'views/components/news-item.html'
+          }).
+        when('/content/:newsId', {
+            templateUrl: 'views/components/news-detail.html'
+          }).
+        when('/posts', {
+            templateUrl: 'views/components/post-list.html',
+            controller: 'PostsListCtrl'
+          })
+        .when('/posts/:_id', {
+            templateUrl: 'views/components/post-detail.html',
+            controller: 'PostPageCtrl'
+          })
+        .otherwise({
+            redirectTo: '/content'
+          });
+      }]);
+  })();
